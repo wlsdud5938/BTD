76cebc64b8ab9084dc461363a9b754376270105e
wlsdud5938 <43804487+wlsdud5938@users.noreply.github.com>
2019-06-29 08:01:15 +0900

    이동 ai 완성.../?

--- BecomeTheDevil/Assets/Navigation2D/TestScene/Move.cs
+++ BecomeTheDevil/Assets/Navigation2D/TestScene/Move.cs
@@ -3,26 +3,22 @@ using System.Collections;
 
 public class Move : MonoBehaviour
 {
-    void Update()
-    {
-        if (Input.GetMouseButton(0))
-        {
-            Vector3 w = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            GetComponent<NavMeshAgent2D>().destination = w;
-        }
-    }
-    /*
-     
-    Vector3 target;
-    GameObject start;
+    //void Update()
+    //{
+    //    if (Input.GetMouseButton(0))
+    //    {
+    //        Vector3 w = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    //        GetComponent<NavMeshAgent2D>().destination = w;
+    //    }
+    //}     
+    public Vector3 target;
     private void Start()
     {
-        start = GameObject.FindGameObjectWithTag("Finish").gameObject;
-        target = start.transform.position;
+
+        target = GameObject.FindGameObjectWithTag("Finish").gameObject.transform.position;
     }
     void Update()
     {
             GetComponent<NavMeshAgent2D>().destination = target;
     }
-     */
 }
