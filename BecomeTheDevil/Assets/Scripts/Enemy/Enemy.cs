75ca845d9c3ecc52dd9004d649883d8023681589
msmf3 <43058677+msmf3@users.noreply.github.com>
2019-07-13 18:20:38 +0900

    push

---
+++ BecomeTheDevil/Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public Vector2 curLocation;
+    public Vector2 preLocation;
+
+    private Animator enemyAnimator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        preLocation = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
