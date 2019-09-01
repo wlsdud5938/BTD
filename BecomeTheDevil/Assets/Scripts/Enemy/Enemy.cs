5504791be9131d8f80ce0b6e60b1d18e88109961
msmf3 <43058677+msmf3@users.noreply.github.com>
2019-09-01 17:14:18 +0900

    기사애니메이션(idle임시추가버전)
    v

--- BecomeTheDevil/Assets/Scripts/Enemy/Enemy.cs
+++ BecomeTheDevil/Assets/Scripts/Enemy/Enemy.cs
@@ -4,38 +4,44 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    private Vector2 curLocation;
-    private Vector2 preLocation;
+    protected Vector3 curLocation;
+    protected Vector3 preLocation;
 
-    private Animator enemyAnimator;
+    protected Animator enemyAnimator;
 
     // Start is called before the first frame update
-    void Start()
+    protected void Start()
     {
         preLocation = transform.position;
         enemyAnimator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
-    void Update()
+    protected void Update()
+    {
+        checkLR();
+    }
+
+    void checkLR()
     {
         curLocation = transform.position;
-        if(preLocation.x < curLocation.x)
+        if (preLocation.x < curLocation.x)
         {
             //transform.localScale = new Vector3(1, 1, 1);
             enemyAnimator.SetBool("Walking", true);
             enemyAnimator.SetFloat("DirX", 1.0f);
         }
-        else if(preLocation.x > curLocation.x)
+        else if (preLocation.x > curLocation.x)
         {
             //transform.localScale = new Vector3(-1, 1, 1);
             enemyAnimator.SetBool("Walking", true);
             enemyAnimator.SetFloat("DirX", -1.0f);
         }
-        else if(preLocation.x == curLocation.x)
+        else if (preLocation.x == curLocation.x && preLocation.z == curLocation.z)
         {
             enemyAnimator.SetBool("Walking", false);
         }
         preLocation = transform.position;
     }
+
 }
---
+++ BecomeTheDevil/Assets/Scripts/Enemy/Knight.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Knight : MonoBehaviour
+{
+    protected Vector3 curLocation;
+    protected Vector3 preLocation;
+
+    protected Animator enemyAnimator;
+
+    //private bool wasRight = false;
+    private bool wasLeft = false;
+
+    // Start is called before the first frame update
+    protected void Start()
+    {
+        preLocation = transform.position;
+        enemyAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    protected void Update()
+    {
+        checkDir();
+    }
+
+    void checkDir()
+    {
+        curLocation = transform.position;
+        
+        if (preLocation.x < curLocation.x)
+        {
+            //transform.localScale = new Vector3(1, 1, 1);
+            enemyAnimator.SetBool("Walking", true);
+            enemyAnimator.SetFloat("DirX", 1.0f);
+            wasLeft = false;
+            //wasRight = true;
+        }
+        else if (preLocation.x > curLocation.x)
+        {
+            //transform.localScale = new Vector3(-1, 1, 1);
+            enemyAnimator.SetBool("Walking", true);
+            enemyAnimator.SetFloat("DirX", -1.0f);
+            wasLeft = true;
+        }
+
+        if (preLocation.z < curLocation.z && preLocation.x == curLocation.x)
+        {
+            //transform.localScale = new Vector3(1, 1, 1);
+            enemyAnimator.SetBool("Walking", true);
+            if (wasLeft)
+            {
+                enemyAnimator.SetFloat("DirX", 0.0f);
+                enemyAnimator.SetFloat("DirY", -1.0f);
+            }
+            else
+            {
+                enemyAnimator.SetFloat("DirX", 0.0f);
+                enemyAnimator.SetFloat("DirY", 1.0f);
+            }
+            
+        }
+        else if (preLocation.z > curLocation.z && preLocation.x == curLocation.x)
+        {
+            //transform.localScale = new Vector3(-1, 1, 1);
+            enemyAnimator.SetBool("Walking", true);
+            if (wasLeft)
+            {
+                enemyAnimator.SetFloat("DirX", -1.0f);
+                enemyAnimator.SetFloat("DirY", 0.0f);
+            }
+            else
+            {
+                enemyAnimator.SetFloat("DirX",  1.0f);
+                enemyAnimator.SetFloat("DirY", 0.0f);
+            }
+        }
+
+        else if (preLocation.x == curLocation.x && preLocation.z == curLocation.z)
+        {
+            enemyAnimator.SetBool("Walking", false);
+            if (wasLeft)
+            {
+                enemyAnimator.SetFloat("DirX", -1.0f);
+                enemyAnimator.SetFloat("DirY", 0.0f);
+            }
+            else
+            {
+                enemyAnimator.SetFloat("DirX", 1.0f);
+                enemyAnimator.SetFloat("DirY", 0.0f);
+            }
+        }
+
+        preLocation = transform.position;
+    }
+    
+}
