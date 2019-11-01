0f8f51d6b2a51c6b052080d4928a4a680d7a4365
snsoo <38851452+snsoo@users.noreply.github.com>
2019-11-01 23:07:44 +0900

    UI프리팹 추가 및 스프라이트 수정
    타겟 체력바 및 메세지박스 추가

---
+++ BecomeTheDevil/Assets/Scripts/UI/MessageManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MessageManager : MonoBehaviour
+{
+    public GameObject[] messageObjects;       // 메세지 박스 오브젝트들 3개(순서 중요)
+    public Text[] messageTexts;             // 메세지 텍스트 3개(순서 중요)
+
+    public void ShowMessage(string message)
+    {
+
+    }
+}
