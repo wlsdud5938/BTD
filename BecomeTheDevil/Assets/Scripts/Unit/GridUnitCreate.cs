d12ff87317aee51aa54d006496b90f3f233b4503
wlsdud5938 <wlsduddud23@gmail.com>
2019-12-03 16:15:20 +0900

    grid 주석

--- BecomeTheDevil/Assets/Scripts/Unit/GridUnitCreate.cs
+++ BecomeTheDevil/Assets/Scripts/Unit/GridUnitCreate.cs
@@ -4,17 +4,16 @@ using UnityEngine;
 
 public class GridUnitCreate : MonoBehaviour
 {
-    public GameObject target;
-    public GameObject unit;
-    Vector3 truePos;
-    public float gridSize;
-    public RaycastHit[] allHit;
-    public int length;
-    public int curUnitCount;
-    public int curMaxUnit;
-    public RoomInfo curRoom;
-    bool otherColl = true;
-    int mapNum = -1;
+    public GameObject target;   //보여지는 유닛의 위치를 지정해줍니다.(현재 조정이 살짝 필요합니다.)
+    public GameObject unit; //보여질 유닛들의 부모오브젝트
+    public float gridSize;  //칸의 크기를 지정합니다(기본으로 1로 지정합니다)
+    public RaycastHit[] allHit; //마우스가 클릭한 모든 것을 배열에 저장합니다.
+    public int length;  //allHit의 길이를 알아보기위해 public으로 뺐습니다.
+    public int curUnitCount;    //현재 방의 유닛수를 지정해줍니다.
+    public int curMaxUnit;  //현재 방의 최대 유닛수를 알려줍니다.
+    public RoomInfo curRoom;    //현재방의 roominfo를 받아옵니다.
+    bool otherColl = true;  //마우스가 클릭했을때 그 위치에 다른 객체가 존재하는지 판단하는 bool변수입니다.
+    int mapNum = -1;    //map이 몇번째 allHit에 존재하는지 알려주는 변수입니다. -1일때 map이 잡히지 않은것 입니다.
     // Start is called before the first frame update
     void Start()
     {
@@ -25,12 +24,13 @@ public class GridUnitCreate : MonoBehaviour
     void Update()
     {
         //Raycast from the mouse position
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        allHit = Physics.RaycastAll(ray, 20.0f);
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);    //스크린에 클릭한 위치를 잡아줍니다.
+        allHit = Physics.RaycastAll(ray, 20.0f);    //allHit에 물리적으로 잡힌 모든 오브젝트를 allHit에 저장합니다.
         //Here acceptableLayer is set to my Ground layer that's what I want to check against
         length = allHit.Length;
         mapNum = -1;
         otherColl = true;
+        //allHit에 적이나 아군 플레이어 벽이 잡힌다면 유닛이미지는 따라오지 않습니다.
         for(int i=0;i<length;i++)
         {
             string tag = allHit[i].collider.tag;
@@ -42,7 +42,7 @@ public class GridUnitCreate : MonoBehaviour
             if (tag == "Map")
                 mapNum = i;
         }
-        
+        //현재 위치에 맞춰 유닛 이미지를 이동시킵니다.
         if (otherColl && mapNum != -1)
         {
             
