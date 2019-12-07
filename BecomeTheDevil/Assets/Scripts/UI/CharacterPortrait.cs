3115e668f77ce9750b1a416c8ddef5ab5d3b9d21
KamJaTang <43845624+KamJaTang@users.noreply.github.com>
2019-12-08 03:45:19 +0900

    hpbar

--- BecomeTheDevil/Assets/Scripts/UI/CharacterPortrait.cs
+++ BecomeTheDevil/Assets/Scripts/UI/CharacterPortrait.cs
@@ -2,22 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class CharacterPortrait : MonoBehaviour
 {
     public GameObject player;
 
+    public Slider hpBar;
+
     public string currentPlayerState;
     private string playerState;
 
-    public Sprite[] playerPannels;
+    public Sprite[] playerPannels;  // 0 그린 스텐딩, 1 화이트, 2 그린 슬라임
+
+    public float currentHP;
+    public float maxHP;
+    public float slidingTimeHP = 0.5f;
 
     // Start is called before the first frame update
     void Start()
     {
         playerState = "green";
-        //currentPlayerState = "green";
-
+   
         player = GameObject.Find("Player");
     }
 
@@ -26,12 +32,22 @@ public class CharacterPortrait : MonoBehaviour
     {
         currentPlayerState = player.GetComponent<Moving>().state;
         if (playerState != currentPlayerState) SetPannels();
+
+        if(maxHP != player.GetComponent<Health>().maxHP)
+        {
+            maxHP = player.GetComponent<Health>().maxHP;
+            SetMaxHP(maxHP);
+        }
+        if (currentHP != player.GetComponent<Health>().currentHP)
+        {
+            currentHP = player.GetComponent<Health>().currentHP;
+            SetCurrentHP(currentHP);
+        }
     }
 
 
     void SetPannels()
     {
-        Debug.Log("it is Done");
         playerState = currentPlayerState;
         if (playerState == "green")
         {
@@ -43,5 +59,21 @@ public class CharacterPortrait : MonoBehaviour
         else {
             gameObject.GetComponent<Image>().sprite = playerPannels[2];
         }
+    }
+
+    void SetMaxHP(float maxHP)
+    {
+        DOTween.To(() => hpBar.GetComponentInChildren<RectTransform>().sizeDelta,
+            x => hpBar.GetComponentInChildren<RectTransform>().sizeDelta = x,
+            new Vector2(maxHP * 2, 20f), slidingTimeHP);
+        DOTween.To(() => hpBar.maxValue, x => hpBar.maxValue = x, maxHP, slidingTimeHP);
+        //hpBar.GetComponentInChildren<RectTransform>().sizeDelta = new Vector3(maxHP * 2, 20f);
+        //hpBar.maxValue = maxHP;
+    }
+
+    void SetCurrentHP(float currentHP)
+    {
+        DOTween.To(() => hpBar.value, x => hpBar.value = x, currentHP, slidingTimeHP);
+        //hpBar.value = currentHP;
     }
 }
