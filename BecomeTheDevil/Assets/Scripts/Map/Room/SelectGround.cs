cf032329b16b75948b12313a22bbd52cf55baf44
wlsdud5938 <wlsduddud23@gmail.com>
2019-12-17 13:15:30 +0900

    맵 바닥 랜덤 생성

---
+++ BecomeTheDevil/Assets/Scripts/Map/Room/SelectGround.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectGround : MonoBehaviour
+{
+    public List<GameObject> groundList = new List<GameObject>();
+    // Start is called before the first frame update
+    void Awake()
+    {
+        int random = Random.Range(0, groundList.Count);
+        GameObject child = Instantiate(groundList[random], transform) as GameObject;
+        child.transform.parent = transform;
+    }
+}
